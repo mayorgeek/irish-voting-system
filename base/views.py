2d5d79d99b88abeebfa58ee3096c15674a1ac6a9
Isaac Adepoju <isaacadepoju2004@gmail.com>
2023-02-20 02:35:57 +0100

    completed the ui for all student and admin dashboards

--- base/views.py
+++ base/views.py
@@ -26,3 +26,45 @@ def admin_register(request):
     context = {}
     return render(request, 'admin_register.html', context)
 
+
+def account_page(request):
+    context = {}
+    return render(request, 'account_page.html', context)
+
+
+def start_voting(request):
+    context = {}
+    return render(request, 'start_voting.html', context)
+
+
+def view_all_voting(request):
+    context = {}
+    return render(request, 'view_all_voting.html', context)
+
+
+def add_new_candidate(request):
+    context = {}
+    return render(request, 'add_new_candidate.html', context)
+
+
+def view_all_candidates(request):
+    context = {}
+    return render(request, 'view_all_candidates.html', context)
+
+
+def view_all_results(request):
+    context = {}
+    return render(request, 'view_all_results.html', context)
+
+
+def view_all_students(request):
+    context = {}
+    return render(request, 'view_all_students.html', context)
+
+
+def voting_page(request):
+    context = {}
+    return render(request, 'voting_page.html', context)
+
+
+
--- votingsystem/urls.py
+++ votingsystem/urls.py
@@ -23,4 +23,12 @@ urlpatterns = [
     path('student/register/', views.student_register, name="student_register"),
     path('admin/login/', views.admin_login, name="admin_login"),
     path('admin/register/', views.admin_register, name="admin_register"),
+    path('admin/', views.account_page, name="account_page"),
+    path('admin/start-voting/', views.start_voting, name="start_voting"),
+    path('admin/view-all-voting/', views.view_all_voting, name="view_all_voting"),
+    path('admin/add-new-candidate/', views.add_new_candidate, name="add_new_candidate"),
+    path('admin/view-all-candidates/', views.view_all_candidates, name="view_all_candidates"),
+    path('admin/view-all-results/', views.view_all_results, name="view_all_results"),
+    path('admin/view-all-students/', views.view_all_students, name="view_all_students"),
+    path('voting-page/', views.voting_page, name="voting_page"),
 ]
