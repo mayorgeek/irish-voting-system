5754c52b5e66ea8f8a8489c98ccc80a070aabcc6
Isaac Adepoju <isaacadepoju2004@gmail.com>
2023-02-27 14:10:52 +0100

    completed results feature

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,15 @@
 from django.forms import ModelForm
-from .models import Candidate
+from .models import Candidate,Voting
 
 
 class CandidateForm(ModelForm):
     class Meta:
         model = Candidate
         fields = ['name', 'category', 'rank']
+
+
+class VotingForm(ModelForm):
+    class Meta:
+        model = Voting
+        fields = ['label']
+
--- base/views.py
+++ base/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
-from .forms import CandidateForm
-from .models import Candidate
+from .forms import CandidateForm,VotingForm
+from .models import Candidate,Voting,User,Vote
 
 
 # Create your views here.
@@ -56,12 +56,21 @@ def account_page(request):
 
 def start_voting(request):
 
+    if request.method == 'POST':
+        form = VotingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('view_all_voting')
+
     context = {}
     return render(request, 'start_voting.html', context)
 
 
 def view_all_voting(request):
-    context = {}
+
+    votings = Voting.objects.all()
+
+    context = {'votings': votings}
     return render(request, 'view_all_voting.html', context)
 
 
@@ -74,7 +83,6 @@ def add_new_candidate(request):
             messages.error(request, "This Rank is Already Taken!")
         else:
             form = CandidateForm(request.POST)
-            form.save()
             if form.is_valid():
                 form.save()
                 return redirect('view_all_candidates')
@@ -103,20 +111,81 @@ def remove_candidate(request, pk):
 
 
 def view_all_results(request):
-    context = {}
+
+    voting_labels = Voting.objects.all()
+    results = []
+
+    for voting in voting_labels:
+        candidates = Candidate.objects.filter(category=voting.label)
+
+        first_candidate = ""
+        second_candidate = ""
+        third_candidate = ""
+        fourth_candidate = ""
+
+        first_candidate_votes = 0
+        second_candidate_votes = 0
+        third_candidate_votes = 0
+        fourth_candidate_votes = 0
+
+        try:
+            first_candidate = candidates[0]
+            first_candidate_votes = Vote.objects.filter(preferred_candidate=candidates[0].name).count()
+        except:
+            messages.error(request, "No first candidate vote yet")
+
+        try:
+            second_candidate = candidates[1]
+            second_candidate_votes = Vote.objects.filter(preferred_candidate=candidates[1].name).count()
+        except:
+            messages.error(request, "No second candidate vote yet")
+
+        try:
+            third_candidate = candidates[2]
+            third_candidate_votes = Vote.objects.filter(preferred_candidate=candidates[2].name).count()
+        except:
+            messages.error(request, "No third candidate vote yet")
+
+        try:
+            fourth_candidate = candidates[3]
+            fourth_candidate_votes = Vote.objects.filter(preferred_candidate=candidates[3].name).count()
+        except:
+            messages.error(request, "No fourth candidate vote yet")
+
+        data = {
+            'label': voting.label,
+            'candidates': candidates,
+            'first_candidate': first_candidate,
+            'second_candidate': second_candidate,
+            'third_candidate': third_candidate,
+            'fourth_candidate': fourth_candidate,
+            'first_candidate_votes': first_candidate_votes,
+            'second_candidate_votes': second_candidate_votes,
+            'third_candidate_votes': third_candidate_votes,
+            'fourth_candidate_votes': fourth_candidate_votes,
+        }
+
+        results.append(data)
+
+    context = {'results': results}
+
     return render(request, 'view_all_results.html', context)
 
 
 def view_all_students(request):
-    context = {}
+
+    users = User.objects.get(role="student")
+
+    context = {'users': users}
     return render(request, 'view_all_students.html', context)
 
 
 def voting_page(request):
 
-    labels =
+    candidates = Candidate.objects.all()
+    categories = Voting.objects.all()
 
-    context = {}
+    context = {'candidates': candidates, 'categories': categories}
     return render(request, 'voting_page.html', context)
 
 
